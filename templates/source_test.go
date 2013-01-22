908025514fbbe07de7f44aaf4e83bfd6b890d1dd
Jeroen Vermeulen <jtv@canonical.com>
2013-01-22 18:34:46 +0700

    Copyright notices, templates, and README.

---
+++ templates/source_test.go
@@ -0,0 +1,14 @@
+// Copyright 2013 Canonical Ltd.  This software is licensed under the
+// GNU Lesser General Public License version 3 (see the file COPYING).
+
+package gomaasapi
+
+import (
+	"launchpad.net/gocheck"
+)
+
+
+// TODO: Replace with real test functions.  Give them real names.
+func (suite *USSOTestSuite) TestXXX(c *gocheck.C) {
+	c.Check(2 + 2, gocheck.Equals, 4)
+}
