fed24c89df2dd64a59d0aac8f54615da210de272
Raphael Badin <raphael.badin@canonical.com>
2013-01-24 10:23:18 +0100

    Fix license headers.

--- testing.go
+++ testing.go
@@ -1,5 +1,5 @@
 // Copyright 2013 Canonical Ltd.  This software is licensed under the
-// GNU Affero General Public License version 3 (see the file LICENSE).
+// GNU Lesser General Public License version 3 (see the file COPYING).
 
 package gomaasapi
 
