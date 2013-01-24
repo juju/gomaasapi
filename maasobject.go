ec426943626ccb39d1ddf2acd4be0d8f1e735231
Jeroen Vermeulen <jtv@canonical.com>
2013-01-24 19:12:15 +0700

    Stubs for MAASModel.

--- maasobject.go
+++ maasobject.go
@@ -7,7 +7,30 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/url"
 )
+
+
+// MAASModel represents a model object as returned by the MAAS API.  This is
+// a special kind of MAASObject.  A MAAS API call will usually return either
+// a MAASModel or a list of MAASModels.  (The list itself will be wrapped in
+// a MAASObject).
+type MAASModel interface {
+	// Resource URI for this object.
+	URL() string
+	// Retrieve this model object.
+	Get() (MAASModel, error)
+	// Write this model object.
+	Post(params url.Values) (MAASModel, error)
+	// Update this model object with the given values.
+	Update(params url.Values) (MAASModel, error)
+	// Delete this model object.
+	Delete() error
+	// Invoke a GET-based method on this model object.
+	CallGet(operation string, params url.Values) (MAASObject, error)
+	// Invoke a POST-based method on this model object.
+	CallPost(operation string, params url.Values) (MAASObject, error)
+}
 
 
 // MAASObject is a wrapper around a JSON structure which provides
@@ -21,10 +44,13 @@ import (
 // to get the value as a float64.  If it's a list, call GetArray() to get
 // a slice of MAASObjects.  To read any given item from the slice, you'll
 // need to "Get" that as the right type as well.
+// There is one exception: a MAASModel is really a special kind of map,
+// so you can read it as either.
 // Reading a null item is also an error.  So before you try obj.Get*(),
 // first check that obj != nil.
 type MAASObject interface {
-	// Type of this value: "string", "float64", "map", "array", or "bool".
+	// Type of this value:
+	// "string", "float64", "map", "model", "array", or "bool".
 	Type() string
 	// Read as string.
 	GetString() (string, error)
@@ -32,6 +58,8 @@ type MAASObject interface {
 	GetFloat64() (float64, error)
 	// Read object as map.
 	GetMap() (map[string]MAASObject, error)
+	// Read object as MAAS model object.
+	GetModel() (MAASModel, error)
 	// Read list as array.
 	GetArray() ([]MAASObject, error)
 	// Read as bool.
@@ -44,12 +72,17 @@ type MAASObject interface {
 // hard-coded variable type.
 // So for each JSON type, there is a separate implementation of MAASObject
 // that converts only to that type.  Any other conversion is an error.
+// One type is special: maasModel is a model object.  It behaves just like
+// a maasMap if you want it to, but it also implements MAASModel.
 type maasString string
 type maasFloat64 float64
 type maasMap map[string]MAASObject
+type maasModel maasMap
 type maasArray []MAASObject
 type maasBool bool
 
+
+const resource_uri = "resource_uri"
 
 // Internal: turn a completely untyped json.Unmarshal result into a
 // MAASObject (with the appropriate implementation of course).
@@ -70,6 +103,11 @@ func maasify(value interface{}) MAASObject {
 		for key, value := range original {
 			result[key] = maasify(value)
 		}
+		if _, ok := result[resource_uri]; ok {
+			// If the map contains "resource-uri", we can treat
+			// it as a model object.
+			return maasModel(result)
+		}
 		return maasMap(result)
 	case []interface{}:
 		original := value.([]interface{})
@@ -81,7 +119,8 @@ func maasify(value interface{}) MAASObject {
 	case bool:
 		return maasBool(value.(bool))
 	}
-	panic(fmt.Sprintf("Unknown JSON type, can't be converted to MAASObject: %v", value))
+	msg := fmt.Sprintf("Unknown JSON type, can't be converted to MAASObject: %v", value)
+	panic(msg)
 }
 
 
@@ -103,98 +142,130 @@ func failConversion(wanted_type string, obj MAASObject) error {
 }
 
 
-// Make an empty map for returning with an error.
-func blankMap() map[string]MAASObject {
-	return make(map[string]MAASObject, 0)
-}
-
-
-// Make an empty array for returning with an error.
-func blankArray() []MAASObject {
-	return make([]MAASObject, 0)
-}
-
-
-// Implementations of MAASObject types follow.
-
-func (maasString) Type() string { return "string" }
-func (obj maasString) GetString() (string, error) { return string(obj), nil }
-func (obj maasString) GetFloat64() (float64, error) {
-	return 0, failConversion("float64", obj)
-}
-func (obj maasString) GetMap() (map[string]MAASObject, error) {
-	return blankMap(), failConversion("map", obj)
-}
-func (obj maasString) GetArray() ([]MAASObject, error) {
-	return blankArray(), failConversion("map", obj)
-}
-func (obj maasString) GetBool() (bool, error) {
-	return false, failConversion("bool", obj)
-}
-
-func (maasFloat64) Type() string { return "float64" }
-func (obj maasFloat64) GetString() (string, error) {
+// Error return values for failure to convert to string.
+func failString(obj MAASObject) (string, error) {
 	return "", failConversion("string", obj)
 }
-func (obj maasFloat64) GetFloat64() (float64, error) { return float64(obj), nil }
-func (obj maasFloat64) GetMap() (map[string]MAASObject, error) {
-	return blankMap(), failConversion("map", obj)
-}
-func (obj maasFloat64) GetArray() ([]MAASObject, error) {
-	return blankArray(), failConversion("map", obj)
-}
-func (obj maasFloat64) GetBool() (bool, error) {
-	return false, failConversion("bool", obj)
-}
-
-func (maasMap) Type() string { return "map" }
-func (obj maasMap) GetString() (string, error) {
-	return "", failConversion("string", obj)
-}
-func (obj maasMap) GetFloat64() (float64, error) {
+// Error return values for failure to convert to float64.
+func failFloat64(obj MAASObject) (float64, error) {
 	return 0.0, failConversion("float64", obj)
 }
+// Error return values for failure to convert to map.
+func failMap(obj MAASObject) (map[string]MAASObject, error) {
+	return make(map[string]MAASObject, 0), failConversion("map", obj)
+}
+// Error return values for failure to convert to model.
+func failModel(obj MAASObject) (MAASModel, error) {
+	return maasModel{}, failConversion("model", obj)
+}
+// Error return values for failure to convert to array.
+func failArray(obj MAASObject) ([]MAASObject, error) {
+	return make([]MAASObject, 0), failConversion("array", obj)
+}
+// Error return values for failure to convert to bool.
+func failBool(obj MAASObject) (bool, error) {
+	return false, failConversion("bool", obj)
+}
+
+
+// MAASObject implementation for maasString.
+func (maasString) Type() string { return "string" }
+func (obj maasString) GetString() (string, error) { return string(obj), nil }
+func (obj maasString) GetFloat64() (float64, error) { return failFloat64(obj) }
+func (obj maasString) GetMap() (map[string]MAASObject, error) { return failMap(obj) }
+func (obj maasString) GetModel() (MAASModel, error) { return failModel(obj) }
+func (obj maasString) GetArray() ([]MAASObject, error) { return failArray(obj) }
+func (obj maasString) GetBool() (bool, error) { return failBool(obj) }
+
+// MAASObject implementation for maasFloat64.
+func (maasFloat64) Type() string { return "float64" }
+func (obj maasFloat64) GetString() (string, error) { return failString(obj) }
+func (obj maasFloat64) GetFloat64() (float64, error) { return float64(obj), nil }
+func (obj maasFloat64) GetMap() (map[string]MAASObject, error) { return failMap(obj) }
+func (obj maasFloat64) GetModel() (MAASModel, error) { return failModel(obj) }
+func (obj maasFloat64) GetArray() ([]MAASObject, error) { return failArray(obj) }
+func (obj maasFloat64) GetBool() (bool, error) { return failBool(obj) }
+
+// MAASObject implementation for maasMap.
+func (maasMap) Type() string { return "map" }
+func (obj maasMap) GetString() (string, error) { return failString(obj) }
+func (obj maasMap) GetFloat64() (float64, error) { return failFloat64(obj) }
 func (obj maasMap) GetMap() (map[string]MAASObject, error) {
 	return (map[string]MAASObject)(obj), nil
 }
-func (obj maasMap) GetArray() ([]MAASObject, error) {
-	return blankArray(), failConversion("map", obj)
+func (obj maasMap) GetModel() (MAASModel, error) { return failModel(obj) }
+func (obj maasMap) GetArray() ([]MAASObject, error) { return failArray(obj) }
+func (obj maasMap) GetBool() (bool, error) { return failBool(obj) }
+
+
+// MAASObject implementation for maasModel.
+func (maasModel) Type() string { return "model" }
+func (obj maasModel) GetString() (string, error) { return failString(obj) }
+func (obj maasModel) GetFloat64() (float64, error) { return failFloat64(obj) }
+func (obj maasModel) GetMap() (map[string]MAASObject, error) {
+	return (map[string]MAASObject)(obj), nil
 }
-func (obj maasMap) GetBool() (bool, error) {
-	return false, failConversion("bool", obj)
+func (obj maasModel) GetModel() (MAASModel, error) {
+	return maasModel(obj), nil
+}
+func (obj maasModel) GetArray() ([]MAASObject, error) { return failArray(obj) }
+func (obj maasModel) GetBool() (bool, error) { return failBool(obj) }
+
+
+// MAASModel implementation for maasModel.
+
+func (obj maasModel) URL() string {
+	contents, err := obj.GetMap()
+	if err != nil {
+		panic("Unexpected failure converting maasModel to maasMap.")
+	}
+	url, err := contents[resource_uri].GetString()
+	if err != nil {
+		panic("Unexpected failure reading maasModel's URL.")
+	}
+	return url
 }
 
+var NotImplemented = errors.New("Not implemented")
+
+func (obj maasModel) Get() (MAASModel, error) {
+	return maasModel{}, NotImplemented
+}
+
+func (obj maasModel) Post(params url.Values) (MAASModel, error) {
+	return maasModel{}, NotImplemented
+}
+
+func (obj maasModel) Update(params url.Values) (MAASModel, error) {
+	return maasModel{}, NotImplemented
+}
+
+func (obj maasModel) Delete() error { return NotImplemented }
+
+func (obj maasModel) CallGet(operation string, params url.Values) (MAASObject, error) {
+	return nil, NotImplemented
+}
+func (obj maasModel) CallPost(operation string, params url.Values) (MAASObject, error) {
+	return nil, NotImplemented
+}
+
+
+// MAASObject implementation for maasArray.
 func (maasArray) Type() string { return "array" }
-func (obj maasArray) GetString() (string, error) {
-	return "", failConversion("string", obj)
-}
-func (obj maasArray) GetFloat64() (float64, error) {
-	return 0.0, failConversion("float64", obj)
-}
-func (obj maasArray) GetMap() (map[string]MAASObject, error) {
-	return blankMap(), failConversion("map", obj)
-}
+func (obj maasArray) GetString() (string, error) { return failString(obj) }
+func (obj maasArray) GetFloat64() (float64, error) { return failFloat64(obj) }
+func (obj maasArray) GetMap() (map[string]MAASObject, error) { return failMap(obj) }
+func (obj maasArray) GetModel() (MAASModel, error) { return failModel(obj) }
 func (obj maasArray) GetArray() ([]MAASObject, error) {
 	return ([]MAASObject)(obj), nil
 }
-func (obj maasArray) GetBool() (bool, error) {
-	return false, failConversion("bool", obj)
-}
+func (obj maasArray) GetBool() (bool, error) { return failBool(obj) }
 
+// MAASObject implementation for maasBool.
 func (maasBool) Type() string { return "bool" }
-func (obj maasBool) GetString() (string, error) {
-	return "", failConversion("string", obj)
-}
-func (obj maasBool) GetFloat64() (float64, error) {
-	return 0.0, failConversion("float64", obj)
-}
-func (obj maasBool) GetMap() (map[string]MAASObject, error) {
-	return blankMap(), failConversion("map", obj)
-}
-func (obj maasBool) GetArray() ([]MAASObject, error) {
-	return blankArray(), failConversion("map", obj)
-}
-func (obj maasBool) GetBool() (bool, error) {
-	return bool(obj), nil
-}
-
+func (obj maasBool) GetString() (string, error) { return failString(obj) }
+func (obj maasBool) GetFloat64() (float64, error) { return failFloat64(obj) }
+func (obj maasBool) GetMap() (map[string]MAASObject, error) { return failMap(obj) }
+func (obj maasBool) GetModel() (MAASModel, error) { return failModel(obj) }
+func (obj maasBool) GetArray() ([]MAASObject, error) { return failArray(obj) }
+func (obj maasBool) GetBool() (bool, error) { return bool(obj), nil }
