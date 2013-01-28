3fd70128e65ec676ed9423b59577eb5a1ad21a28
Jeroen Vermeulen <jtv@canonical.com>
2013-01-28 16:36:33 +0700

    CamelCase, not underscores.

--- util.go
+++ util.go
@@ -8,9 +8,9 @@ import (
 )
 
 // Join a base URL and a subpath together.
-// Regardless of whether base_url ends in a trailing slash (or even multiple
+// Regardless of whether baseURL ends in a trailing slash (or even multiple
 // trailing slashes), or whether there are any leading slashes at the begining
 // of path, the two will always be joined together by a single slash.
-func JoinURLs(base_url, path string) string {
-	return strings.TrimRight(base_url, "/") + "/" + strings.TrimLeft(path, "/")
+func JoinURLs(baseURL, path string) string {
+	return strings.TrimRight(baseURL, "/") + "/" + strings.TrimLeft(path, "/")
 }
