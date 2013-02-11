cd55225653f5a7537e5d04518b8112b5d2b0113e
Jeroen Vermeulen <jtv@canonical.com>
2013-02-11 12:45:22 +0700

    Reduce JSONObject tests' reliance on casting.

--- jsonobject_test.go
+++ jsonobject_test.go
@@ -20,25 +20,31 @@ func (suite *JSONObjectSuite) TestMaasifyConvertsNil(c *C) {
 // maasify() converts strings.
 func (suite *JSONObjectSuite) TestMaasifyConvertsString(c *C) {
 	const text = "Hello"
-	c.Check(string(maasify(Client{}, text).(jsonString)), Equals, text)
+	out, err := maasify(Client{}, text).GetString()
+	c.Assert(err, IsNil)
+	c.Check(out, Equals, text)
 }
 
 // maasify() converts float64 numbers.
 func (suite *JSONObjectSuite) TestMaasifyConvertsNumber(c *C) {
 	const number = 3.1415926535
-	c.Check(float64(maasify(Client{}, number).(jsonFloat64)), Equals, number)
+	num, err := maasify(Client{}, number).GetFloat64()
+	c.Assert(err, IsNil)
+	c.Check(num, Equals, number)
 }
 
 // maasify() converts array slices.
 func (suite *JSONObjectSuite) TestMaasifyConvertsArray(c *C) {
 	original := []interface{}{3.0, 2.0, 1.0}
-	output := maasify(Client{}, original).(jsonArray)
+	output, err := maasify(Client{}, original).GetArray()
+	c.Assert(err, IsNil)
 	c.Check(len(output), Equals, len(original))
 }
 
 // When maasify() converts an array slice, the result contains JSONObjects.
 func (suite *JSONObjectSuite) TestMaasifyArrayContainsJSONObjects(c *C) {
-	arr := maasify(Client{}, []interface{}{9.9}).(jsonArray)
+	arr, err := maasify(Client{}, []interface{}{9.9}).GetArray()
+	c.Assert(err, IsNil)
 	var entry JSONObject
 	entry = arr[0]
 	c.Check((float64)(entry.(jsonFloat64)), Equals, 9.9)
@@ -47,13 +53,16 @@ func (suite *JSONObjectSuite) TestMaasifyArrayContainsJSONObjects(c *C) {
 // maasify() converts maps.
 func (suite *JSONObjectSuite) TestMaasifyConvertsMap(c *C) {
 	original := map[string]interface{}{"1": "one", "2": "two", "3": "three"}
-	output := maasify(Client{}, original).(jsonMap)
+	output, err := maasify(Client{}, original).GetMap()
+	c.Assert(err, IsNil)
 	c.Check(len(output), Equals, len(original))
 }
 
 // When maasify() converts a map, the result contains JSONObjects.
 func (suite *JSONObjectSuite) TestMaasifyMapContainsJSONObjects(c *C) {
-	mp := maasify(Client{}, map[string]interface{}{"key": "value"}).(jsonMap)
+	jsonobj := maasify(Client{}, map[string]interface{}{"key": "value"})
+	mp, err := jsonobj.GetMap()
+	c.Assert(err, IsNil)
 	var entry JSONObject
 	entry = mp["key"]
 	c.Check((string)(entry.(jsonString)), Equals, "value")
@@ -65,61 +74,84 @@ func (suite *JSONObjectSuite) TestMaasifyConvertsMAASObject(c *C) {
 		"resource_uri": "http://example.com/foo",
 		"size":         "3",
 	}
-	output := maasify(Client{}, original).(jsonMAASObject)
-	c.Check(len(output.jsonMap), Equals, len(original))
-	c.Check((string)(output.jsonMap["size"].(jsonString)), Equals, "3")
+	output, err := maasify(Client{}, original).GetMAASObject()
+	c.Assert(err, IsNil)
+	mp := output.(jsonMAASObject).jsonMap
+	c.Check(len(mp), Equals, len(original))
+	size, err := mp["size"].GetString()
+	c.Assert(err, IsNil)
+	c.Check(size, Equals, "3")
 }
 
 // maasify() passes its client to a MAASObject it creates.
-func (suite *JSONObjectSuite) TestMaasifyPassesInfoToMAASObject(c *C) {
+func (suite *JSONObjectSuite) TestMaasifyPassesClientToMAASObject(c *C) {
 	client := Client{}
 	original := map[string]interface{}{"resource_uri": "/foo"}
-	output := maasify(client, original).(jsonMAASObject)
-	c.Check(output.client, Equals, client)
+	output, err := maasify(client, original).GetMAASObject()
+	c.Assert(err, IsNil)
+	c.Check(output.(jsonMAASObject).client, Equals, client)
 }
 
 // maasify() passes its client into an array of MAASObjects it creates.
-func (suite *JSONObjectSuite) TestMaasifyPassesInfoIntoArray(c *C) {
+func (suite *JSONObjectSuite) TestMaasifyPassesClientIntoArray(c *C) {
 	client := Client{}
 	obj := map[string]interface{}{"resource_uri": "/foo"}
 	list := []interface{}{obj}
-	output := maasify(client, list).(jsonArray)
-	c.Check(output[0].(jsonMAASObject).client, Equals, client)
+	jsonobj, err := maasify(client, list).GetArray()
+	c.Assert(err, IsNil)
+	out, err := jsonobj[0].GetMAASObject()
+	c.Assert(err, IsNil)
+	c.Check(out.(jsonMAASObject).client, Equals, client)
 }
 
 // maasify() passes its client into a map of MAASObjects it creates.
-func (suite *JSONObjectSuite) TestMaasifyPassesInfoIntoMap(c *C) {
+func (suite *JSONObjectSuite) TestMaasifyPassesClientIntoMap(c *C) {
 	client := Client{}
 	obj := map[string]interface{}{"resource_uri": "/foo"}
 	mp := map[string]interface{}{"key": obj}
-	output := maasify(client, mp).(jsonMap)
-	c.Check(output["key"].(jsonMAASObject).client, Equals, client)
+	jsonobj, err := maasify(client, mp).GetMap()
+	c.Assert(err, IsNil)
+	out, err := jsonobj["key"].GetMAASObject()
+	c.Assert(err, IsNil)
+	c.Check(out.(jsonMAASObject).client, Equals, client)
 }
 
 // maasify() passes its client all the way down into any MAASObjects in the
 // object structure it creates.
-func (suite *JSONObjectSuite) TestMaasifyPassesInfoAllTheWay(c *C) {
+func (suite *JSONObjectSuite) TestMaasifyPassesClientAllTheWay(c *C) {
 	client := Client{}
 	obj := map[string]interface{}{"resource_uri": "/foo"}
 	mp := map[string]interface{}{"key": obj}
 	list := []interface{}{mp}
-	output := maasify(client, list).(jsonArray)
-	maasobj := output[0].(jsonMap)["key"]
-	c.Check(maasobj.(jsonMAASObject).client, Equals, client)
+	jsonobj, err := maasify(client, list).GetArray()
+	c.Assert(err, IsNil)
+	outerMap, err := jsonobj[0].GetMap()
+	c.Assert(err, IsNil)
+	out, err := outerMap["key"].GetMAASObject()
+	c.Check(out.(jsonMAASObject).client, Equals, client)
 }
 
 // maasify() converts Booleans.
 func (suite *JSONObjectSuite) TestMaasifyConvertsBool(c *C) {
-	c.Check(bool(maasify(Client{}, true).(jsonBool)), Equals, true)
-	c.Check(bool(maasify(Client{}, false).(jsonBool)), Equals, false)
+	t, err := maasify(Client{}, true).GetBool()
+	c.Assert(err, IsNil)
+	f, err := maasify(Client{}, false).GetBool()
+	c.Assert(err, IsNil)
+	c.Check(t, Equals, true)
+	c.Check(f, Equals, false)
 }
 
 // Parse takes you from a JSON blob to a JSONObject.
 func (suite *JSONObjectSuite) TestParseMaasifiesJSONBlob(c *C) {
 	blob := []byte("[12]")
 	obj, err := Parse(Client{}, blob)
-	c.Check(err, IsNil)
-	c.Check(float64(obj.(jsonArray)[0].(jsonFloat64)), Equals, 12.0)
+	c.Assert(err, IsNil)
+
+	arr, err := obj.GetArray()
+	c.Assert(err, IsNil)
+	out, err := arr[0].GetFloat64()
+	c.Assert(err, IsNil)
+	c.Check(out, Equals, 12.0)
 }
 
 // String-type JSONObjects convert only to string.
